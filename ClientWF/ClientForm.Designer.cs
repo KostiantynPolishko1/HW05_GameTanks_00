4c293250347698660241ba0359bfd731f3ae230e
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2024-04-12 22:22:29 +0300

    1. Managed rotation 90 degres of picture tank using class Graphics.

--- ClientWF/ClientForm.Designer.cs
+++ ClientWF/ClientForm.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Client";
+            pcBxPlayer = new PictureBox();
+            btnMoveUp = new Button();
+            btnMoveDown = new Button();
+            btnRotateCW = new Button();
+            btnRotateCCW = new Button();
+            ((System.ComponentModel.ISupportInitialize)pcBxPlayer).BeginInit();
+            SuspendLayout();
+            // 
+            // pcBxPlayer
+            // 
+            pcBxPlayer.Location = new Point(200, 200);
+            pcBxPlayer.Name = "pcBxPlayer";
+            pcBxPlayer.Size = new Size(100, 100);
+            pcBxPlayer.SizeMode = PictureBoxSizeMode.Zoom;
+            pcBxPlayer.TabIndex = 0;
+            pcBxPlayer.TabStop = false;
+            // 
+            // btnMoveUp
+            // 
+            btnMoveUp.Location = new Point(12, 12);
+            btnMoveUp.Name = "btnMoveUp";
+            btnMoveUp.Size = new Size(100, 30);
+            btnMoveUp.TabIndex = 1;
+            btnMoveUp.Text = "MoveUp";
+            btnMoveUp.UseVisualStyleBackColor = true;
+            btnMoveUp.Click += btnMoveUp_Click;
+            // 
+            // btnMoveDown
+            // 
+            btnMoveDown.Location = new Point(12, 54);
+            btnMoveDown.Name = "btnMoveDown";
+            btnMoveDown.Size = new Size(100, 30);
+            btnMoveDown.TabIndex = 2;
+            btnMoveDown.Text = "MoveDown";
+            btnMoveDown.UseVisualStyleBackColor = true;
+            // 
+            // btnRotateCW
+            // 
+            btnRotateCW.Location = new Point(12, 96);
+            btnRotateCW.Name = "btnRotateCW";
+            btnRotateCW.Size = new Size(100, 30);
+            btnRotateCW.TabIndex = 3;
+            btnRotateCW.Text = "Rotate(+)";
+            btnRotateCW.UseVisualStyleBackColor = true;
+            btnRotateCW.Click += btnRotateCW_Click;
+            // 
+            // btnRotateCCW
+            // 
+            btnRotateCCW.Location = new Point(12, 132);
+            btnRotateCCW.Name = "btnRotateCCW";
+            btnRotateCCW.Size = new Size(100, 30);
+            btnRotateCCW.TabIndex = 4;
+            btnRotateCCW.Text = "Rotate(-)";
+            btnRotateCCW.UseVisualStyleBackColor = true;
+            btnRotateCCW.Click += btnRotateCCW_Click;
+            // 
+            // ClientForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(500, 500);
+            Controls.Add(btnRotateCCW);
+            Controls.Add(btnRotateCW);
+            Controls.Add(btnMoveDown);
+            Controls.Add(btnMoveUp);
+            Controls.Add(pcBxPlayer);
+            Name = "ClientForm";
+            Text = "Client";
+            ((System.ComponentModel.ISupportInitialize)pcBxPlayer).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pcBxPlayer;
+        private Button btnMoveUp;
+        private Button btnMoveDown;
+        private Button btnRotateCW;
+        private Button btnRotateCCW;
     }
 }
--- ClientWF/ClientForm.cs
+++ ClientWF/ClientForm.cs
@@ -1,10 +1,81 @@
+using System.Drawing;
+
 namespace ClientWF
 {
     public partial class ClientForm : Form
     {
+        private Bitmap bmp;
+
         public ClientForm()
         {
             InitializeComponent();
+            bmp = new Bitmap("A:\\OneDrive - ITSTEP\\NetworkProg\\Projects\\HW05_GameTanks_00\\tank.png");
+
+            setMainColor();
+
+            Color backColor = bmp.GetPixel(1, 1);
+            bmp.MakeTransparent(backColor);
+
+            pcBxPlayer.Image = bmp;
+        }
+
+        private void setMainColor()
+        {
+            for (int i = 0; i != bmp.Width; i++)
+            {
+                for (int j = 0; j != bmp.Height; j++)
+                {
+                    if (bmp.GetPixel(i, j) == Color.FromArgb(0, 255, 0))
+                    {
+                        bmp.SetPixel(i, j, Color.Brown);
+                    }
+                }
+            }
+        }
+
+        private Bitmap rotateBitMap(int angle, Bitmap bmp)
+        {
+            (int width, int height) = getSizeRotation(angle, bmp);
+
+            Bitmap rBmp = new Bitmap(width, height);
+
+            Graphics g = Graphics.FromImage(rBmp);
+            g.TranslateTransform(width / 2, height / 2);
+            g.RotateTransform(angle);
+            g.TranslateTransform(-bmp.Width / 2, -bmp.Height / 2);
+            g.DrawImage(bmp, 0, 0);
+
+            return rBmp;
+        }
+
+        private (int, int) getSizeRotation(int angle, Bitmap bmp)
+        {
+            int width = (int)(Math.Abs(bmp.Width * Math.Cos(Math.PI * angle / 180)) + Math.Abs(bmp.Height * Math.Sin(Math.PI * angle / 180)));
+            int height = (int)(Math.Abs(bmp.Height * Math.Cos(Math.PI * angle / 180)) + Math.Abs(bmp.Width * Math.Sin(Math.PI * angle / 180)));
+
+            return (width, height);
+        }
+
+        private void rotateTank(int angle)
+        {
+            bmp = rotateBitMap(angle, bmp);
+            pcBxPlayer.Image = bmp;
+            pcBxPlayer.Refresh();
+        }
+
+        private void btnMoveUp_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnRotateCW_Click(object sender, EventArgs e)
+        {
+            rotateTank(90);
+        }
+
+        private void btnRotateCCW_Click(object sender, EventArgs e)
+        {
+            rotateTank(-90);
         }
     }
 }
