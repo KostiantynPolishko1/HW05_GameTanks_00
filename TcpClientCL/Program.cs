e6cc48ddabb55abc6d1d64495ec1e68f87812fd3
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-04-22 23:01:14 +0300

    1. Set up simple communication between Server and Client using string msg.

---
+++ TcpClientCL/Program.cs
@@ -0,0 +1,39 @@
+﻿using Library.Models;
+using System.Net.Sockets;
+
+namespace TcpClientCL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            using (TcpClient client = new TcpClient())
+            {
+                try
+                {
+                    client.Connect(new ConnectIPEndP().getIpeP());
+                    Console.WriteLine("Client Start!");
+
+                    StreamReader reader = new StreamReader(client.GetStream());
+                    StreamWriter writer = new StreamWriter(client.GetStream());
+
+                    while (true)
+                    {
+                        Console.Write("enter msg: ");
+                        writer.WriteLine(Console.ReadLine());
+                        writer.Flush();
+
+                        Console.Write("answer msg: ");
+                        Console.WriteLine(reader.ReadLine());
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+                finally
+                {
+                    client.Close();
+                }
+
+            }
+        }
+    }
+}
---
+++ TcpServerCL/Program.cs
@@ -0,0 +1,39 @@
+﻿using Library.Models;
+using System.Net.Sockets;
+
+namespace TcpServerCL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(new ConnectIPEndP().getIpeP());
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server Start!");
+
+                using(TcpClient client = server.AcceptTcpClient())
+                {
+                    StreamReader reader = new StreamReader(client.GetStream());
+                    StreamWriter writer = new StreamWriter(client.GetStream());
+
+                    while (true)
+                    {
+                        Console.Write("answer msg: ");
+                        Console.WriteLine(reader.ReadLine());
+
+                        Console.Write("enter msg: ");
+                        writer.WriteLine(Console.ReadLine());
+                        writer.Flush();
+                    }
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally 
+            { 
+                server.Stop(); 
+            }
+        }
+    }
+}
