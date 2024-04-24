be216745c2ff9c5eac8169afc6ad6740726194bd
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-04-24 14:40:32 +0300

    1 Created project WinForms with Bitmap of player.

--- ClientWF/ClientForm.Designer.cs
+++ ClientWF/ClientForm.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Client";
+            SuspendLayout();
+            // 
+            // ClientForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(400, 400);
+            KeyPreview = true;
+            Name = "ClientForm";
+            Text = "Client";
+            Paint += ClientForm_Paint;
+            KeyDown += ClientForm_KeyDown;
+            ResumeLayout(false);
         }
 
         #endregion
